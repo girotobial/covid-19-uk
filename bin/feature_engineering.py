9fd7d2424a44a59b59edf93594122f7aea78261a
opensomersault <abrobinson1907@gmail.com>
2020-03-13 20:03:41 +0000

    feat: add derivative function

--- bin/feature_engineering.py
+++ bin/feature_engineering.py
@@ -13,7 +13,20 @@ def growth_ratio(iterable):
             ratio = val / iterable[i - 1]
 
         growth_list.append(ratio)
-    return growth_list          
+    return growth_list
+
+
+def calculate_derivative(iterable):
+    change_list = []
+    for i, val in enumerate(iterable):
+        if i == 0:
+            delta = 0
+
+        if i > 0:
+            delta = val - iterable[i - 1]
+
+        change_list.append(delta)
+    return change_list
 
 
 def main():
@@ -22,6 +35,13 @@ def main():
     dataframe = pd.read_csv(data_path / 'DailyConfirmedCases.csv')
 
     dataframe['GrowthRatio'] = growth_ratio(dataframe['CumCases'])
+    dataframe['SecondDerivativeCases'] = calculate_derivative(
+        dataframe['CMODateCount']
+    )
+    dataframe.to_csv(
+        data_path / 'DailyConfirmedCasesWithFeatures.csv',
+        index=False
+    )
 
 
 if __name__ == '__main__':
