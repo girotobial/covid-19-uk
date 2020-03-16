9eb9c8d6f128a106bde7583dc726fc6f551cdcc5
Alex Robinson <alexander.robinson@dunlopatl.co.uk>
2020-03-16 13:45:03 +0000

    feat: add plot generation to main experiment

--- run_expirment.py
+++ run_expirment.py
@@ -1,9 +1,11 @@
 import bin.feature_engineering as feature_engineering
 import bin.update_data as update_data
+import bin.plots as plots
 
 def main():
     update_data.main()
     feature_engineering.main()
+    plots.main()
 
 if __name__ == '__main__':
     main()
