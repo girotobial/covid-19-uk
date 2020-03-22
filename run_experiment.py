ff34ceb48793ebe1d766d9c7f0993cade5a46688
opensomersault <abrobinson1907@gmail.com>
2020-03-22 08:39:37 +0000

    feat: add prints to show progress

--- run_experiment.py
+++ run_experiment.py
@@ -3,8 +3,10 @@ import bin.update_data as update_data
 import bin.plots as plots
 
 def main():
+    print('Updating...')
     update_data.main()
     feature_engineering.main()
+    print('Plotting...')
     plots.main()
 
 if __name__ == '__main__':
