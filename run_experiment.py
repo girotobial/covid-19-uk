bc46cfcee1d5c13db876bb6657cde35c3e79eb94
opensomersault <abrobinson1907@gmail.com>
2020-04-02 14:56:05 +0100

    removed data update as PHE have removed february, to be refactored

--- run_experiment.py
+++ run_experiment.py
@@ -1,10 +1,7 @@
 import bin.feature_engineering as feature_engineering
-import bin.update_data as update_data
 import bin.plots as plots
 
 def main():
-    print('Updating...')
-    update_data.main()
     feature_engineering.main()
     print('Plotting...')
     plots.main()
