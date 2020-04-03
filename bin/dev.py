6db1e4afd38ab8dcf0cf3b21b05882b7025eac19
opensomersault <abrobinson1907@gmail.com>
2020-04-03 14:16:30 +0100

    2020-04-03 update

--- bin/dev.py
+++ bin/dev.py
@@ -10,35 +10,14 @@ data = pd.read_csv(
     index_col='DateVal'
 )
 
-data.columns
-# %%
-data['rolling_new_cases'] = data['CMODateCount'].rolling(7).sum()
-#data = data[data['CumCases'] > 10]
 # %%
 sns.set(style='ticks', context='notebook')
 plt.plot(
     data['CumCases'],
-    data['rolling_new_cases'],
+    data['CumDeaths']
 )
-plt.xscale('log')
 plt.yscale('log')
-plt.scatter(
-    data['CumCases'][-1],
-    data['rolling_new_cases'][-1],
-    marker='o'
-)
-_, x_max = plt.xlim()
-plt.xlim(10, x_max)
-plt.ylim(10, x_max)
-plt.ylabel('New Confirmed Cases (in the Past Week')
-plt.xlabel('Total Confirmed Cases')
-plt.title('Trajectory of Covid-19 Confirmed Cases (UK)')
-plt.text(
-    10,
-    1,
-    'Based on work by Aatish Bhatia & Minute Physics\nhttps://aatishb.com/covidtrends/ ')
-plt.grid()
-sns.despine()
-
-
-# %%
+plt.xscale('log')
+plt.ylim(1, 10**5)
+plt.xlim(1, 10**5)
+plt.show()
