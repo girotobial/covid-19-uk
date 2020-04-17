7aec8aa31bc814460e872b31451a11987a2e1e9a
opensomersault <abrobinson1907@gmail.com>
2020-04-17 14:17:03 +0100

    remove update functions

--- run_experiment.py
+++ run_experiment.py
@@ -1,11 +1,8 @@
-from bin.update_data import main as update
 from bin.feature_engineering import main as features
 from bin.plots import main as plot
 
 
 def main():
-    print('Updating...')
-    update()
     print('Features...')
     features()
     print('Plotting...')
