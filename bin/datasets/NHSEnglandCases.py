ab64f5892c85292429a6fba33b52c4b21afcb76a
opensomersault <abrobinson1907@gmail.com>
2020-04-24 18:34:31 +0100

    add new methods

--- bin/datasets/NHSEnglandCases.py
+++ bin/datasets/NHSEnglandCases.py
@@ -9,16 +9,50 @@ class NHSEnglandCases:
     def __init__(self, URL, date_cols=['Specimen date']):
         self.csv = self._download_csv(URL)
         self._date_cols = date_cols
+        self.df = self._dataframe()
 
     def _download_csv(self, URL):
+        # Download using URL
         csv = requests.get(URL, stream=True).content
+
+        # Decode from string
         csv = csv.decode('utf-8')
+
         return csv
     
-    @property
-    def dataframe(self):
+    def _dataframe(self):
+        '''Returns downloaded csv as a pandas dataframe'''
         df = pd.read_csv(
             StringIO(self.csv),
             parse_dates=self._date_cols
         )
+
+        return df
+    
+    def _filter_area_type(self, _type):
+        df = self.df
+        return df[df['Area type'] == _type]
+
+    def nation(self, nation=None):
+        df = self._filter_area_type('Nation')
+
+        if nation is not None:
+            df = df[df['Area Name'] == nation]
+
+        return df
+
+    def region(self, region=None):
+        df = self._filter_area_type('Region')
+
+        if region is not None:
+            df = df[df['Area Name'] == region]
+        
+        return df
+   
+    def utla(self, authority=None):
+        df = self._filter_area_type('Upper tier local authority')
+
+        if authority is not None:
+            df = df[df['Area Name'] == authority]
+        
         return df
