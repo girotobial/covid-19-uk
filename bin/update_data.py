f506f45a78dcc4854170c9c5dbb10d7a24d275f4
opensomersault <abrobinson1907@gmail.com>
2020-04-24 14:06:19 +0100

    implement arcgis download function

--- bin/update_data.py
+++ bin/update_data.py
@@ -3,6 +3,22 @@ from pathlib import Path
 import pandas as pd
 
 
+def arcgis_download(public_data_id, data_path):
+    # Setup gis
+    gis = GIS()
+    data_item = gis.content.get(public_data_id)
+    
+    # Download data
+    data_item.download(save_path=data_path)
+
+    # Import and convert to csv
+    df = pd.read_excel(data_path / 'DailyConfirmedCases.xlsx')
+    df.to_csv(data_path / 'DailyConfirmedCases.csv', index=False)
+
+    # Delete xlsx file
+    Path(data_path / 'DailyConfirmedCases.xlsx').unlink()
+
+
 def main():
     # Setup gis
     public_data_id = 'e5fd11150d274bebaaf8fe2a7a2bda11'
