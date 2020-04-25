ebe3b76fb48f654365f8b10cfe5e1b334c1f6d34
opensomersault <abrobinson1907@gmail.com>
2020-04-25 11:47:18 +0100

    add update to enable england data to be updated

--- run_experiment.py
+++ run_experiment.py
@@ -1,8 +1,10 @@
 from bin.feature_engineering import main as features
 from bin.plots import main as plot
-
+from bin.update_data import main as update_data
 
 def main():
+    print('Updating data')
+    update_data()
     print('Features...')
     features()
     print('Plotting...')
