7550370221dd84e54152f69d829bee04e94eaefb
opensomersault <abrobinson1907@gmail.com>
2020-04-25 18:10:47 +0100

    add init

--- bin/plot/base.py
+++ bin/plot/base.py
@@ -2,11 +2,20 @@
 # Defines base class for all plots
 
 from abc import ABC, abstractmethod
+import matplotlib.pyplot as plt
 
 class ABCPlot(ABC):
-    def __init__(self, dataframe):
-        self.df = dataframe
-    
+    def __init__(self, figsize=None):
+        if figsize is None:
+            figsize = (9.75, 6)
+        fig, ax = plt.subplots(figsize=figsize)
+        self.fig = fig
+        self.ax = ax
+
+    @abstractmethod
+    def plot(self):
+        pass
+
     @abstractmethod
     def show(self):
         pass
