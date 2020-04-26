4aa7ec864cffd25c72d21ad5086e5418acbb5ecd
opensomersault <abrobinson1907@gmail.com>
2020-04-26 20:03:22 +0100

    refactor: growth ratio method

--- bin/plot/growth_factor.py
+++ bin/plot/growth_factor.py
@@ -6,14 +6,17 @@ from ..calc import growth_ratio
 
 
 class GrowthFactor(ABCPlot):
-    def __init__(self, date_col, cases_col,  data=None):
+    def __init__(self, date_col, cases_col, data=None):
         self._dates_col = date_col
         self._cases_col = cases_col
         self.data = data
         super().__init__()
 
-    def _growth_ratio(self):
-        return growth_ratio(self._cases_col)
+    def _growth_ratio(self, iterable):
+        '''
+        Implements growth ratio function as a method.
+        '''
+        return growth_ratio(iterable)
     
     def _parse_inputs(self):
         # TODO
