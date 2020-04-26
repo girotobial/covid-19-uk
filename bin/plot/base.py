72afbf1cdc4bc48a76e3dd4b6de30258042a1c9e
opensomersault <abrobinson1907@gmail.com>
2020-04-26 19:50:59 +0100

    refactor: reverted to use pyplot and figsize now default value

--- bin/plot/base.py
+++ bin/plot/base.py
@@ -2,14 +2,13 @@
 # Defines base class for all plots
 
 from abc import ABC, abstractmethod
-import matplotlib as mplt
+import matplotlib.pyplot as plt
 
 class ABCPlot(ABC):
-    def __init__(self, figsize=None):
-        if figsize is None:
-            figsize = (9.75, 6)
-        self.fig = mplt.figure.Figure()
-        self.ax = mplt.axes.Axes()
+    def __init__(self, figsize=(9.75, 6)):
+        fig, ax = plt.subplots(figsize=figsize)
+        self.fig = fig
+        self.ax = ax
 
     @abstractmethod
     def plot(self):
