c53c84cbe5c46041b2cd479c45f1e1fb46f3f623
opensomersault <abrobinson1907@gmail.com>
2020-04-26 09:46:45 +0100

    add methods

--- bin/plot/growth_factor.py
+++ bin/plot/growth_factor.py
@@ -2,16 +2,32 @@
 # Defines the GrowthFactor plot class
 
 from .base import ABCPlot
+from ..calc import growth_ratio
 
 
 class GrowthFactor(ABCPlot):
     def __init__(self, date_col, cases_col,  data=None):
-        self._dates_col = dates_col
+        self._dates_col = date_col
         self._cases_col = cases_col
         self.data = data
         super().__init__()
+
+    def _growth_ratio(self):
+        return growth_ratio(self._cases_col)
     
     def _parse_inputs(self):
+        # TODO
+        pass
+
+    def plot(self):
+        # TODO
+        pass
+
+    def show(self):
+        # TODO
+        pass
+
+    def save(self):
         # TODO
         pass
 
